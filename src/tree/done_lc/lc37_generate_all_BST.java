850dfdf3ef89eddf4a2b386a2486b32a606ba99f
abhinavdubey13 <abhinavdubey13@gmail.com>
2023-06-25 19:37:32 +0530

    .

--- src/tree/done_lc/lc37_generate_all_BST.java
+++ src/tree/done_lc/lc37_generate_all_BST.java
@@ -1,4 +1,4 @@
-package tree;
+package tree.done_lc;
 
 import models.TreeNode;
 import java.util.*;
@@ -6,11 +6,16 @@ import java.util.*;
 /**
  * leetcode id : 95
  *
- * Given an integer n, return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
+ * 
+ * https://leetcode.com/problems/unique-binary-search-trees-ii/
+ * 
+ * Given an integer n, return all the structurally unique BST's (binary search
+ * trees), which has exactly n nodes of unique values from 1 to n.
+ * 
+ * Return the answer in any order.
  *
  *
  */
-
 
 /**
  *
@@ -19,11 +24,14 @@ import java.util.*;
  * approach :
  * ============
  *
- * start by noting that 1..n is the in-order traversal for any BST with nodes 1 to n
+ * start by noting that 1..n is the in-order traversal for any BST with nodes 1
+ * to n
  *
- * So if we pick i-th node as my root, the left subtree will contain elements 1 to (i-1), and the right subtree will contain elements (i+1) to n
+ * So if we pick i-th node as my root, the left subtree will contain elements 1
+ * to (i-1), and the right subtree will contain elements (i+1) to n
  *
- * Then use recursive calls to get back all possible trees for left and right subtrees and combine them in all possible ways with the root
+ * Then use recursive calls to get back all possible trees for left and right
+ * subtrees and combine them in all possible ways with the root
  *
  * ===========
  * TC=O()
@@ -33,10 +41,14 @@ import java.util.*;
  *
  */
 
-
 public class lc37_generate_all_BST {
-}
 
+    public static void main(String[] args) {
+        // new lc37_generate_all_BST_rev1().find(3);
+        new lc37_generate_all_BST_soln().generateTrees(3);
+
+    }
+}
 
 class lc37_generate_all_BST_soln {
 
@@ -61,18 +73,17 @@ class lc37_generate_all_BST_soln {
         List<TreeNode> left, right;
         for (int i = start; i <= end; i++) {
 
+            TreeNode root = new TreeNode(i);
             left = helper(start, i - 1);
             right = helper(i + 1, end);
 
             for (TreeNode lnode : left) {
                 for (TreeNode rnode : right) {
-                    TreeNode root = new TreeNode(i);
                     root.left = lnode;
                     root.right = rnode;
                     list.add(root);
                 }
             }
-
         }
 
         return list;
